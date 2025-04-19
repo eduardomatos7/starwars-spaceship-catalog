c8036950719bc20096a6025e32c150953ded8308
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-04-19 18:38:39 -0300

    feat: cria paginação das naves

--- src/app/espaconaves/[id]/page.tsx
+++ src/app/espaconaves/[id]/page.tsx
@@ -1,30 +1,48 @@
 "use client"
 import Title from '@/components/Title'
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "@/app/page.module.css"
 import Image from 'next/image'
 import spaceships from "@/app/spaceships.json"
+import { useRouter } from "next/navigation"
 
 function Spaceship() {
-    const params = useParams()
+  const router = useRouter()
+  const params = useParams()
     if (typeof params.id !== 'string') return null
     console.log(params.spaceship)
     const spaceship = spaceships.find((item) => item.id === +params.id!)
+    const length = spaceships.length
+    function handleSkip() {
+      router.push(`/espaconaves/${+params.id! + 1}`)
+    }
+    function handlePrev() {
+      router.push(`/espaconaves/${+params.id! - 1}`)
+    }
+
   return (
     <>
       <Title title={spaceship && spaceship.name} />
+      
       {spaceship &&
       <div key={spaceship.id} className={styles.spaceshipContent}>
         <div className={styles.contentImage}>
-        <Image src={spaceship.imageUrl} alt={spaceship.name} width={400} height={250} className={styles.imageSpaceship}/>
+        <Image src={spaceship.imageUrl} 
+        alt={spaceship.name} 
+        width={400} 
+        height={250} 
+        className={styles.imageSpaceship}
+        priority={true}
+        />
         </div>
         <p>Classe: {spaceship.category}</p>
         <p>Modelo: {spaceship.model}</p>
-        <p>{spaceship.description}</p>
-        <div className={styles.containerBtn}>
-          <button className={styles.btnBottom}>Voltar</button>
-          <button className={styles.btnBottom}>Próximo</button>
+        <p className={styles.description}>{spaceship.description}</p>
+        <div className={+params.id > 1 ? styles.containerBtn : styles.right}>
+          {+params.id > 1 && <button className={styles.btnBottom} onClick={handlePrev}>Voltar</button>}
+        {+params.id === length ? "" : <button className={styles.btnBottom} onClick={handleSkip}>Próximo</button>}
+          
           </div>
       </div>
       }
