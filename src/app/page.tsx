62c6ef1637bb2e9529ad2332bcab0e7293702cf7
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-04-18 21:04:24 -0300

    feat: cria tela inicial listando todas as espaçonaves

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
+import { useState } from "react";
+import spaceships from "./spaceships.json"
 
 export default function Home() {
+  console.log(spaceships)
+  const [enableBtn, setEnableBtn] = useState(false)
   return (
-    <div>Hello World</div>
+    <div className={styles.contentTitleBtn}>
+      <h2>Conheça as Naves do Universo Star Wars</h2>
+      <button className={styles.btnSeeAll}
+        onClick={() => setEnableBtn(!enableBtn)}
+      >{!enableBtn ? "Ver todas" : "Ocultar"}
+      </button>
+      {enableBtn &&
+        <div className={styles.spaceshipsContent}>
+          {spaceships.map((item) => (
+            <div key={item.id} className={styles.spaceship}>
+              <h3>{item.name}</h3>
+            </div>
+          ))}
+        </div>
+
+      }
+
+    </div>
   );
 }
